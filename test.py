0333e469fee865810c0319cee62ae8bfad91ab45
m-salari <mslri79kh@gmail.com>
2025-03-18 21:09:31 +0330

    delete test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-
-d = {'name': 'mostafa', 'age': 30, 'city': 'tehran'}
-print(json.dumps(d, indent=1, separators=(",,,,", ":W:"), sort_keys=True ))
-
-# js = '{"name": "mostafa", "age": 30, "city": "tehran"}'
-# print(json.loads(js))
